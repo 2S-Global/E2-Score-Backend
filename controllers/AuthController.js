9afc568f4f97bd9bd225a72f0b04a57cf65931b7
2S-Global <2sglobaltechnologies@gmail.com>
2025-03-25 12:49:05 +0530

    updated respose code

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -35,7 +35,7 @@ export const registerUser = async (req, res) => {
             /*  data: newUser, */
         });
 
-        res.status(201).json({ message: "User created successfully" });
+        res.status(200).json({ message: "User created successfully" });
     } catch (error) {
         res.status(500).json({ message: "Error creating user", error: error.message });
     }
@@ -73,7 +73,7 @@ export const registerCompany = async (req, res) => {
             /*  data: newUser, */
         });
 
-        res.status(201).json({ message: "User created successfully" });
+        res.status(200).json({ message: "User created successfully" });
     } catch (error) {
         res.status(500).json({ message: "Error creating user", error: error.message });
     }
