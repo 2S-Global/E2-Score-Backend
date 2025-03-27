010f694d07cf1df409021863a11936c813bfff77
2S-Global <2sglobaltechnologies@gmail.com>
2025-03-27 17:52:03 +0530

    upload

--- controllers/userVerificationController.js
+++ controllers/userVerificationController.js
@@ -128,7 +128,7 @@ export const verifyPAN = async (req, res) => {
   
       const epicApiResponse = response.data;
   
-      const updatedUser = await UserVerification.findByIdAndUpdate(
+      const updatedUser = await UserCartVerification.findByIdAndUpdate(
         id,
         {
           $set: {
@@ -267,10 +267,130 @@ export const searchUserVerifiedList = async (req, res) => {
       }
 
       res.status(200).json(users);
-      res.status(200).json(users);
+
   } catch (error) {
       console.error("Error fetching verified users:", error);
       res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
+export const verifyPassport = async (req, res) => {
+  try {
+
+    const employer_id = req.userId;
+    if (!employer_id) {
+      return res.status(400).json({ message: "Employer ID is required" });
+  }
+    const { customer_file_number, candidate_name, candidate_dob  } = req.body;
+
+    if (!customer_file_number || !candidate_name || !candidate_dob) {
+      return res.status(400).json({ message: "Passport number and name are required" });
+    }
+
+    const panData = {
+      mode: "sync",
+      data: {
+        customer_file_number,
+        candidate_name,
+        customer_dob,
+        consent: "Y",
+        consent_text:
+          "I hereby declare my consent agreement for fetching my information via ZOOP API",
+      },
+      task_id: "8bbb54f3-d299-4535-b00e-e74d2d5a3997",
+    };
+
+    // Sending request to Zoop API
+    const response = await axios.post(
+      "https://test.zoop.one/api/v1/in/identity/passport/advance",
+      panData,
+      {
+        headers: {
+          "app-id": "67b8252871c07100283cedc6",
+          "api-key": "52HD084-W614E0Q-JQY5KJG-R8EW1TW",
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    const passportApiResponse = response.data;
+
+
+      const updatedUser = await UserCartVerification.findByIdAndUpdate(
+        employer_id,
+        {
+          $set: {
+            passport_response: passportApiResponse,
+            status:"1"
+          }
+        },
+        { new: true } 
+      );
+      
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(200).json({
+      message: "Passport verification failed",
+      error: error.response ? error.response.data : error.message,
+    });
+  }
+};
+
+export const verifyDL = async (req, res) => {
+  try {
+    const { customer_dl_number, candidate_name, candidate_dob,userId } = req.body;
+
+    if (!customer_dl_number || !candidate_name || !candidate_dob) {
+      return res.status(400).json({ message: "DL number, name, and DOB are required" });
+    }
+
+    const dlData = {
+      mode: "sync",
+      data: {
+        customer_dl_number,
+        candidate_name,
+        candidate_dob,
+        consent: "Y",
+        consent_text:
+          "I hereby declare my consent agreement for fetching my information via ZOOP API"
+      },
+      task_id: "f26eb21e-4c35-4491-b2d5-41fa0e545a34"
+    };
+
+    // Sending request to Zoop API
+    const response = await axios.post(
+      "https://test.zoop.one/api/v1/in/identity/dl/advance",
+      dlData,
+      {
+        headers: {
+          "app-id": "67b8252871c07100283cedc6",
+          "api-key": "52HD084-W614E0Q-JQY5KJG-R8EW1TW",
+          "Content-Type": "application/json"
+        },
+        timeout: 10000, // 10 seconds timeout
+        maxRedirects: 10
+      }
+    );
+
+    const dlApiResponse = response.data;
+
+    const updatedUser = await UserCartVerification.findByIdAndUpdate(
+      userId,
+      {
+        $set: {
+          dl_response: dlApiResponse,
+          status:"1"
+        }
+      },
+      { new: true } 
+    );
+
+
+    res.status(200).json(response.data);
+  } catch (error) {
+    res.status(500).json({
+      message: "DL verification failed",
+      error: error.response ? error.response.data : error.message
+    });
   }
 };
 
