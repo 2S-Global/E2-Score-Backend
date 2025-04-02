30d37b471942c99248bef7101f6b9e923db6c718
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-02 12:00:16 +0530

    Update userVerificationCartController.js

--- controllers/userVerificationCartController.js
+++ controllers/userVerificationCartController.js
@@ -169,7 +169,7 @@ export const getUserVerificationCartByEmployer = async (req, res) => {
         const employer_id = req.userId;
         const verificationCharge = 50;
 
-        const userCarts = await UserCartVerification.find({ employer_id, is_del: false,is_paid:0 });
+        const userCarts = await UserCartVerification.find({ employer_id, is_del: false });
 
         let overallTotalVerifications = 0;
         let overallSubtotal = 0;
