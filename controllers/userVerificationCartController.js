ade9ff6bddc34bc5809d359d3334e6de70b1014c
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-02 10:34:14 +0530

    Update userVerificationCartController.js

--- controllers/userVerificationCartController.js
+++ controllers/userVerificationCartController.js
@@ -244,5 +244,42 @@ export const getPaidUserVerificationCartByEmployer = async (req, res) => {
     }
 };
 
+export const deleteUser = async (req, res) => {
+    try {
+      const { id } = req.body;
+  
+      // Validate ID
+      if (!id) {
+        return res.status(400).json({ message: "ID is required" });
+      }
+  
+      if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(400).json({ message: "Invalid ID format" });
+      }
+  
+     
+      const updatedUser = await UserCartVerification.findByIdAndUpdate(
+        id,
+        { $set: { is_del: true } }, 
+        { new: true }
+      );
+  
+      if (!updatedUser) {
+        return res.status(404).json({ message: "User not found" });
+      }
+  
+      return res.status(200).json({
+        message: "User marked as deleted successfully",
+        updatedUser,
+      });
+  
+    } catch (error) {
+      return res.status(500).json({
+        message: "Error deleting user",
+        error: error.message,
+      });
+    }
+  };
+
 
 
