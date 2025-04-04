224ed42b231fdaa74315ad054191a4b6cf991881
2S-Global <2sglobaltechnologies@gmail.com>
2025-04-04 13:24:11 +0530

    added pdf

--- models/userVerificationModel.js
+++ models/userVerificationModel.js
@@ -1,119 +1,124 @@
 import mongoose from "mongoose";
 
 const userVerificationSchema = new mongoose.Schema(
-    {
-        employer_id: {
-            type: mongoose.Schema.Types.ObjectId,
-            ref: 'User',
-            required: true
-        },
-        candidate_name: {
-            type: String,
-            required: true,
-        },
-        candidate_email: {
-            type: String,
-            required: true,
-        },
-        candidate_mobile: {
-            type: String,
-        },
-        candidate_dob: {
-            type: String,
-            required: true,
-        },
-        candidate_address: {
-            type: String,
-        },
-        candidate_gender: {
-            type: String, // Fixed typo
-        },
-         pan_name: {
-            type: String, // Fixed typo
-        },
-         pan_number: {
-            type: String, // Fixed typo
-        },
-        pan_response: { 
-            type: Object
-        },
-        pan_image: {
-            type: String, // Ensure marks is stored properly
-        },
-         aadhar_name: {
-            type: String, // Fixed typo
-        },
-        aadhar_number: {
-            type: String, // Fixed typo
-        },
-        aadhaar_response: { 
-            type: Object
-        },
-        aadhar_image: {
-            type: String, // Ensure marks is stored properly
-        },
-        dl_name: {
-        type: String, // Fixed typo
-        },
-        dl_number: {
-            type: String, // Fixed typo
-        },
-
-        dl_response: { 
-            type: Object
-        },
-        dl_image: {
-            type: String, // Ensure marks is stored properly
-        },
-         passport_name: {
-            type: String, // Fixed typo
-        },
-        passport_file_number: {
-            type: String, // Fixed typo
-        },
-        passport_response: { 
-            type: Object
-        },
-        passport_image: {
-            type: String, // Ensure marks is stored properly
-        },
-         epic_name: {
-            type: String, // Fixed typo
-        },
-        epic_number: {
-            type: String, // Fixed typo
-        },
-        epic_response: { 
-            type: Object
-        },
-        epic_image: {
-            type: String, // Ensure marks is stored properly
-        },
-        updatedAt: {
-            type: Date,
-            default: Date.now,
-        },
-        
-        is_del: {
-            type: Boolean,
-            default: false,
-        },
-         is_paid: { 
-        type: Number
-      
-        },
-     
-          all_verified:{
-             type: Number,
-            default: 0,
-    }
+  {
+    employer_id: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    pdf_url: {
+      type: String,
+    },
+    candidate_name: {
+      type: String,
+      required: true,
+    },
+    candidate_email: {
+      type: String,
+      required: true,
+    },
+    candidate_mobile: {
+      type: String,
+    },
+    candidate_dob: {
+      type: String,
+      required: true,
+    },
+    candidate_address: {
+      type: String,
+    },
+    candidate_gender: {
+      type: String, // Fixed typo
+    },
+    pan_name: {
+      type: String, // Fixed typo
+    },
+    pan_number: {
+      type: String, // Fixed typo
+    },
+    pan_response: {
+      type: Object,
+    },
+    pan_image: {
+      type: String, // Ensure marks is stored properly
+    },
+    aadhar_name: {
+      type: String, // Fixed typo
+    },
+    aadhar_number: {
+      type: String, // Fixed typo
+    },
+    aadhaar_response: {
+      type: Object,
+    },
+    aadhar_image: {
+      type: String, // Ensure marks is stored properly
+    },
+    dl_name: {
+      type: String, // Fixed typo
+    },
+    dl_number: {
+      type: String, // Fixed typo
     },
 
-    {
-        timestamps: true,
-    }
+    dl_response: {
+      type: Object,
+    },
+    dl_image: {
+      type: String, // Ensure marks is stored properly
+    },
+    passport_name: {
+      type: String, // Fixed typo
+    },
+    passport_file_number: {
+      type: String, // Fixed typo
+    },
+    passport_response: {
+      type: Object,
+    },
+    passport_image: {
+      type: String, // Ensure marks is stored properly
+    },
+    epic_name: {
+      type: String, // Fixed typo
+    },
+    epic_number: {
+      type: String, // Fixed typo
+    },
+    epic_response: {
+      type: Object,
+    },
+    epic_image: {
+      type: String, // Ensure marks is stored properly
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+
+    is_del: {
+      type: Boolean,
+      default: false,
+    },
+    is_paid: {
+      type: Number,
+    },
+
+    all_verified: {
+      type: Number,
+      default: 0,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
 );
 
-const UserVerification = mongoose.model("UserVerificationOrder", userVerificationSchema);
+const UserVerification = mongoose.model(
+  "UserVerificationOrder",
+  userVerificationSchema
+);
 
 export default UserVerification;
--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import express from 'express';
-import cors from 'cors';
-import helmet from 'helmet';
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
 
-import db from './config/db.js';
+import db from "./config/db.js";
 db();
 
 const app = express();
@@ -13,29 +13,29 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Import routes
-import AuthRouter from './routes/AuthRoutes.js';
-import userSkillsRoutes from './routes/userSkillsRoutes.js';
-import userProjectsRoutes from './routes/userProjectsRoutes.js';
-import userEmploymentRoutes from './routes/userEmploymentRoutes.js';
-import userEducationRoutes from './routes/userEducationRoutes.js';
-import userVerificationRoutes from './routes/userVerificationRoutes.js';
-import userVerificationCartRoutes from './routes/userVerificationCartRoutes.js';
-
+import AuthRouter from "./routes/AuthRoutes.js";
+import userSkillsRoutes from "./routes/userSkillsRoutes.js";
+import userProjectsRoutes from "./routes/userProjectsRoutes.js";
+import userEmploymentRoutes from "./routes/userEmploymentRoutes.js";
+import userEducationRoutes from "./routes/userEducationRoutes.js";
+import userVerificationRoutes from "./routes/userVerificationRoutes.js";
+import userVerificationCartRoutes from "./routes/userVerificationCartRoutes.js";
+import pdfRouter from "./routes/pdfRoutes.js";
 
 // Routes
-app.get('/', (req, res) => {
-    res.send('Welcome to the back end of the E2 Score ');
+app.get("/", (req, res) => {
+  res.send("Welcome to the back end of the E2 Score ");
 });
-app.use('/api/auth', AuthRouter);
-app.use('/api/skills', userSkillsRoutes);
-app.use('/api/projects', userProjectsRoutes);
-app.use('/api/employment', userEmploymentRoutes);
-app.use('/api/education', userEducationRoutes);
-app.use('/api/verify', userVerificationRoutes);
-app.use('/api/usercart', userVerificationCartRoutes);
-
+app.use("/api/auth", AuthRouter);
+app.use("/api/skills", userSkillsRoutes);
+app.use("/api/projects", userProjectsRoutes);
+app.use("/api/employment", userEmploymentRoutes);
+app.use("/api/education", userEducationRoutes);
+app.use("/api/verify", userVerificationRoutes);
+app.use("/api/usercart", userVerificationCartRoutes);
+app.use("/api/pdf", pdfRouter);
 // Start server
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
