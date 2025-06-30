5f021b0d12bd768e5ee0a41ae94b53847f148c6e
2S-Global <2sglobaltechnologies@gmail.com>
2025-06-30 13:01:27 +0530

    refactor: enhance MongoDB connection setup with additional options for improved performance and error handling

--- config/db.js
+++ config/db.js
@@ -1,20 +1,27 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import colors from 'colors';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import colors from "colors";
 
 dotenv.config();
 
 const MONGODB_URI = process.env.MONGO_URL;
 
 const db = async () => {
-    try {
-        mongoose.set('strictQuery', true);
-        const con = await mongoose.connect(MONGODB_URI);
-        console.log(`MongoDB connection established: ${con.connection.host}`.green.bold);
-    } catch (error) {
-        console.error(`Error connecting to MongoDB: ${error.message}`.red.inverse);
-        process.exit(1); // Exit process with failure
-    }
-};  
+  try {
+    mongoose.set("strictQuery", true);
+    const con = await mongoose.connect(MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000,
+      maxPoolSize: 50, // default is 10
+    });
+    console.log(
+      `MongoDB connection established: ${con.connection.host}`.green.bold
+    );
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`.red.inverse);
+    process.exit(1); // Exit process with failure
+  }
+};
 
 export default db;
