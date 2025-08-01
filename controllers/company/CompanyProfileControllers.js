e62de4ec85654299e26d02b2336b49d39b9f8947
2S-Global <2sglobaltechnologies@gmail.com>
2025-08-01 12:46:16 +0530

    fix: Add missing line in SearchCompanybyCin function to ensure proper company retrieval

--- controllers/company/CompanyProfileControllers.js
+++ controllers/company/CompanyProfileControllers.js
@@ -24,6 +24,7 @@ export const SearchCompanybyCin = async (req, res) => {
 
   try {
     const company = await companylist.findOne({ cinnumber: cin });
+
     if (!company) {
       return res.status(404).json({
         message:
