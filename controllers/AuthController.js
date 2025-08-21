ff6e8196bd372bd0eb851d0e2acaf720b7eba144
Chandra-Sarkar <chandrasarkar26345@gmail.com>
2025-08-21 14:58:31 +0530

    fix: Update user existence check to use CIN number instead of email

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -98,9 +98,9 @@ export const registerCompany = async (req, res) => {
     }
 
     // Check if user already exists
-    const existingUser = await User.findOne({ email });
+    const existingUser = await User.findOne({ cin_number: cin });
     if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(400).json({ message: "User with this CIN number already exists" });
     }
 
     // companylist
