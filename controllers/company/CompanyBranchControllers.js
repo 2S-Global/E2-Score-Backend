9feed8ad6274723e0b7e267f3766425f21703676
Chandra-Sarkar <chandrasarkar26345@gmail.com>
2025-08-26 10:22:07 +0530

    fix: Update getMultipleEmployeeDetails to include pan_number in the response

--- controllers/company/CompanyBranchControllers.js
+++ controllers/company/CompanyBranchControllers.js
@@ -459,7 +459,7 @@ export const getMultipleEmployeeDetails = async (req, res) => {
 
       personalDetails.findOne(
         { user: userId },
-        { permanentAddress: 1, _id: 0 }
+        { permanentAddress: 1, pan_number:1, _id: 0 }
       ).lean(),
 
       Employment.findOne(
