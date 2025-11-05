c2da17d35ca75426ecaf44120a038b971c1495a5
Chandra-Sarkar <chandrasarkar26345@gmail.com>
2025-11-05 16:44:21 +0530

    update

--- controllers/candidate/userdatacontroller.js
+++ controllers/candidate/userdatacontroller.js
@@ -202,8 +202,8 @@ export const getUser = async (req, res) => {
         father_name: personalData.fatherName || "",
         currency: personalData.currentSalary?.currency || "",
         salary: personalData.currentSalary?.salary || 0,
-        experience_yr: personalData.totalExperience?.year || "",
-        experience_month: personalData.totalExperience?.month || "",
+        experience_years: personalData.totalExperience?.year || "",
+        experience_months: personalData.totalExperience?.month || "",
       }),
       gender_name,
       degree: levelName || "",
@@ -314,8 +314,8 @@ export const getUserDetails = async (req, res) => {
       father_name: personalData?.fatherName || "",
       currency: personalData.currentSalary?.currency || "",
       salary: personalData.currentSalary?.salary || 0,
-      experience_yr: personalData.totalExperience?.year || "",
-      experience_month: personalData.totalExperience?.month || "",
+      experience_years: personalData.totalExperience?.year || "",
+      experience_months: personalData.totalExperience?.month || "",
     };
 
     res.status(200).json(result);
