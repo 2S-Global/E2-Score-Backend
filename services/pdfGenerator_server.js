07e7aaf359a42158d7cd426e218efd5c303d3811
2S-Global <2sglobaltechnologies@gmail.com>
2025-11-13 17:22:51 +0530

    update

--- services/pdfGenerator_server.js
+++ services/pdfGenerator_server.js
@@ -6,12 +6,27 @@ import { fileURLToPath } from "url";
 import puppeteer from "puppeteer-core";
 import chromium from "@sparticuz/chromium";
 
+import path from "path";
+import { fileURLToPath } from "url";
+import fs from "fs";
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const generateResumePDF = async (data) => {
   const templatePath = path.join(__dirname, "../templates/resume.ejs");
-  const htmlContent = await ejs.renderFile(templatePath, { ...data });
+
+  // Added Today Started
+  const defaultImagePath = path.join(__dirname, "../public/images/no_user.png");
+  const defaultImageBase64 = fs
+    .readFileSync(defaultImagePath)
+    .toString("base64");
+  const defaultImageDataUrl = `data:image/png;base64,${defaultImageBase64}`;
+
+  const htmlContent = await ejs.renderFile(templatePath, {
+    ...data,
+    defaultImageDataUrl,
+  });
 
   // const browser = await puppeteer.launch();
   const browser = await puppeteer.launch({
@@ -22,6 +37,17 @@ const generateResumePDF = async (data) => {
   });
   const page = await browser.newPage();
   await page.setContent(htmlContent, { waitUntil: "networkidle0" });
+
+  const generatedDate = new Date().toLocaleString("en-IN", {
+    day: "2-digit",
+    month: "short",
+    year: "numeric",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: true, // show AM/PM
+    timeZone: "Asia/Kolkata",
+  });
 
   const pdfBuffer = await page.pdf({
     format: "A4",
@@ -34,8 +60,18 @@ const generateResumePDF = async (data) => {
     printBackground: true,
     displayHeaderFooter: true,
     footerTemplate: `
-    <div style="width: 100%; font-size: 10px; padding: 5px 0; text-align: center; color: #666;">
-      Page <span class="pageNumber"></span> of <span class="totalPages"></span>
+    <div style="
+      width: 100%;
+      font-size: 10px;
+      padding: 5px 30px;
+      color: #666;
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      border-top: 0.5px solid #ccc;
+    ">
+      <div>Generated on: ${generatedDate}</div>
+      <div>Page <span class="pageNumber"></span> of <span class="totalPages"></span></div>
     </div>
   `,
     headerTemplate: `<div></div>`, // empty header
