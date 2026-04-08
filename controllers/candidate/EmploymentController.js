29753433991d901fc228aec755b608ce46be8786
2S-Global <2sglobaltechnologies@gmail.com>
2026-04-08 10:42:31 +0530

    update

--- controllers/candidate/EmploymentController.js
+++ controllers/candidate/EmploymentController.js
@@ -763,7 +763,7 @@ export const getEmploymentDetails = async (req, res) => {
         notice_period: item.NoticePeriod || "",
         remarks: item.remarks || "",
         notice_period_name: noticePeriodMap[item.NoticePeriod] || "",
-        workedInCompany: item.workedInCompany || false,
+        workedInCompany: item.workedInCompany,
       };
     });
 
