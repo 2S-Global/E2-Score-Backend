85861a9f140b9c3920b8efa23993253d4af17fe0
Chandra-Sarkar <chandrasarkar26345@gmail.com>
2026-04-09 16:00:56 +0530

    update

--- models/TestimonialModel.js
+++ models/TestimonialModel.js
@@ -5,6 +5,7 @@ const TestimonialSchema = new mongoose.Schema({
   customer_designation: { type: String ,required:true,trim:true},
   customer_image: { type: String,default:null },
   description: { type: String ,required:true,trim:true},
+  linkedin_url: { type: String, required:true, trim:true },
   is_del: { type: Boolean, default: false },
 },
 {timestamps:true}
