72cacafbaf76e1bbf0859a27b3d058c62f72735b
2S-Global <2sglobaltechnologies@gmail.com>
2026-06-08 17:40:36 +0530

    update

--- controllers/institute/instituteStudentImport.js
+++ controllers/institute/instituteStudentImport.js
@@ -465,6 +465,196 @@ export const addInstituteStudentManually = async (req, res) => {
       instituteId: user.userId
     });
 
+    //  here code Updation starts
+
+    let student;
+    if (existingStudentEmail) {
+
+      student = await InstitueStudent.findByIdAndUpdate(
+        existingStudentEmail._id,
+        {
+          $set: {
+            name,
+            USN,
+            gender,
+            dob: dobYMD,
+            tenTh,
+            twelveTh,
+            // email, // keep this
+            phoneNumber,
+            program: new mongoose.Types.ObjectId(program),
+            admissionYear,
+            instituteId: user.userId,
+            promotedYear: currentYear,
+            promotedSemester:
+              (existingData?.promotedSemester &&
+                existingData.promotedSemester > semesters?.length)
+                ? existingData.promotedSemester
+                : semesters?.length
+          }
+        },
+        { new: true }
+      );
+
+    } else {
+      const student = await InstitueStudent.findOneAndUpdate(
+        {
+          USN,
+          instituteId: user.userId,
+          admissionYear
+        },
+        {
+          $set: {
+            name,
+            USN,
+            gender,
+            dob: dobYMD,
+            tenTh,
+            twelveTh,
+            email,
+            phoneNumber,
+            program: new mongoose.Types.ObjectId(program), // ✅ FIX
+            admissionYear,
+            instituteId: user.userId,
+            promotedYear: currentYear,
+            promotedSemester: (existingData?.promotedSemester && existingData?.promotedSemester > semesters?.length) ? existingData?.promotedSemester : semesters?.length
+          }
+        },
+        {
+          upsert: true,
+          new: true,
+          runValidators: true
+        }
+      );
+
+
+      // User Account Creation Starts from here
+
+      // Check if user already exists
+      const existingUser = await User.findOne({ email, is_del: false });
+      if (existingUser) {
+        return res.status(400).json({ message: "User already exists" });
+      }
+
+      const generatePassword = () => {
+        const chars =
+          "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#";
+        let password = "";
+        for (let i = 0; i < 10; i++) {
+          password += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return password;
+      };
+
+      const newPassword = generatePassword();
+
+      // Hash new password
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+      // Create a new user with hashed password
+      const newUser = new User({
+        name,
+        email,
+        password: hashedPassword,
+        role: 1,
+        phone_number: phoneNumber,
+        usn: USN
+      });
+      await newUser.save();
+
+      await InstitueStudent.findByIdAndUpdate(
+        student._id,
+        {
+          $set: {
+            userCreatedId: newUser._id
+          }
+        },
+        { new: true }
+      );
+
+      const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: "30d",
+      });
+
+      // Send email with login credentials
+      const transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
+
+      const mailOptions = {
+        from: `"Geisil Team" <${process.env.EMAIL_USER}>`,
+        to: email,
+        subject: "Access Credentials for Geisil",
+        html: `
+      <div style="text-align: center; margin-bottom: 20px;">
+    <img src="https://res.cloudinary.com/da4unxero/image/upload/v1765884063/addacademics_asbt5b.jpg" alt="Banner" style="width: 100%; height: auto;" />
+  </div>
+        <p>Dear <strong>${name}</strong>,</p>
+        <p>Greetings from <strong>Global Employability Information Services India Limited</strong>.</p>
+        <p>
+          We are pleased to provide you with access to our newly launched platform,
+          <a href="https://e2-score-updated.vercel.app" target="_blank">https://e2-score-updated.vercel.app</a>,
+          <strong>Geisil</strong> is a comprehensive job and career platform designed for both candidates and companies. Candidates can register, update their professional profiles, and apply to job opportunities. Employers can sign in, post jobs, and verify candidates who have listed their company in their employment details. Institutes also have the ability to verify candidates in a similar way.
+        </p>
+     
+        <p>Your corporate account has been successfully created with the following credentials:</p>
+        <ul>
+          <li><strong>Email:</strong> ${email}</li>
+          <li><strong>Password:</strong> ${newPassword}</li>
+        </ul>
+     
+       <p>Click the link  to verify your email: <a href="${process.env.BACKEND_URL}/api/auth/verify-email/${token}">Verify Email</a></p>
+     
+        <p><strong>Key Features and Benefits of Geisil:</strong></p>
+        <ul>
+          <li>Job Search & Applications: Candidates can explore and apply to a wide range of job opportunities.</li>
+          <li>Profile Management: Build and update a complete professional profile including education, skills, and work experience.</li>
+          <li>Job Posting: Employers and institutes can post jobs and connect with qualified candidates.</li>
+          <li>Candidate Verification: Companies and institutes can verify candidates who list them in their employment or education history.</li>
+          <li>Seamless Communication: Easy interaction between candidates and employers for smoother recruitment.</li>
+          <li>Secure Platform: Data protection and privacy ensured for both candidates and employers.</li>
+        </ul>
+     
+        <p>
+          We are confident that E2 Score will significantly improve your recruitment and job search experience by making the process faster, easier, and more reliable for both candidates and employers.
+        </p>
+     
+        <p>
+          For any assistance with the platform, including login issues or technical support, please contact our support team at:
+        </p>
+        <ul>
+          <li><strong>Email:</strong> <a href="mailto:info@geisil.com">info@geisil.com</a></li>
+          <li><strong>Phone:</strong> 9831823898</li>
+        </ul>
+     
+        <p>Thank you for choosing <strong>Global Employability Information Services India Limited</strong>.</p>
+        <p>We look forward to supporting your Job Searching and Job Posting needs.</p>
+     
+        <br />
+        <p>Sincerely,<br />
+        The Admin Team<br />
+        <strong>Global Employability Information Services India Limited</strong></p>
+ 
+         <div style="text-align: center; margin-top: 30px;">
+      <img src="https://res.cloudinary.com/da4unxero/image/upload/v1746776002/QuikChek%20images/ntvxq8yy2l9de25t1rmu.png%22 alt="Footer" style="width:97px; height: 116px;" />
+    </div>
+      `,
+      };
+
+      await transporter.sendMail(mailOptions);
+
+      // User Account Creation Ends here
+
+    }
+    // Here code Updation ends
+
+    /*
     if (
       existingStudentEmail &&
       existingStudentEmail.USN !== USN
@@ -474,7 +664,9 @@ export const addInstituteStudentManually = async (req, res) => {
         message: "Student with this email already exists."
       });
     }
+    */
 
+    /*
     const student = await InstitueStudent.findOneAndUpdate(
       {
         USN,
@@ -504,6 +696,7 @@ export const addInstituteStudentManually = async (req, res) => {
         runValidators: true
       }
     );
+    */
 
 
     // -----------------------------
@@ -562,128 +755,6 @@ export const addInstituteStudentManually = async (req, res) => {
     audit.push(logEntry);
     createdCount++;
 
-    // User Account Creation Starts from here
-
-    // Check if user already exists
-    const existingUser = await User.findOne({ email, is_del: false });
-    if (existingUser) {
-      return res.status(400).json({ message: "User already exists" });
-    }
-
-    const generatePassword = () => {
-      const chars =
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#";
-      let password = "";
-      for (let i = 0; i < 10; i++) {
-        password += chars.charAt(Math.floor(Math.random() * chars.length));
-      }
-      return password;
-    };
-
-    const newPassword = generatePassword();
-
-    // Hash new password
-    const hashedPassword = await bcrypt.hash(newPassword, 10);
-
-    // Create a new user with hashed password
-    const newUser = new User({
-      name,
-      email,
-      password: hashedPassword,
-      role: 1,
-      phone_number: phoneNumber,
-      usn: USN
-    });
-    await newUser.save();
-
-    await InstitueStudent.findByIdAndUpdate(
-      student._id,
-      {
-        $set: {
-          userCreatedId: newUser._id
-        }
-      },
-      { new: true }
-    );
-
-    const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
-      expiresIn: "30d",
-    });
-
-    // Send email with login credentials
-    const transporter = nodemailer.createTransport({
-      host: process.env.EMAIL_HOST,
-      port: process.env.EMAIL_PORT,
-      secure: true,
-      auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASS,
-      },
-    });
-
-    const mailOptions = {
-      from: `"Geisil Team" <${process.env.EMAIL_USER}>`,
-      to: email,
-      subject: "Access Credentials for Geisil",
-      html: `
-      <div style="text-align: center; margin-bottom: 20px;">
-    <img src="https://res.cloudinary.com/da4unxero/image/upload/v1765884063/addacademics_asbt5b.jpg" alt="Banner" style="width: 100%; height: auto;" />
-  </div>
-        <p>Dear <strong>${name}</strong>,</p>
-        <p>Greetings from <strong>Global Employability Information Services India Limited</strong>.</p>
-        <p>
-          We are pleased to provide you with access to our newly launched platform,
-          <a href="https://e2-score-updated.vercel.app" target="_blank">https://e2-score-updated.vercel.app</a>,
-          <strong>Geisil</strong> is a comprehensive job and career platform designed for both candidates and companies. Candidates can register, update their professional profiles, and apply to job opportunities. Employers can sign in, post jobs, and verify candidates who have listed their company in their employment details. Institutes also have the ability to verify candidates in a similar way.
-        </p>
-     
-        <p>Your corporate account has been successfully created with the following credentials:</p>
-        <ul>
-          <li><strong>Email:</strong> ${email}</li>
-          <li><strong>Password:</strong> ${newPassword}</li>
-        </ul>
-     
-       <p>Click the link  to verify your email: <a href="${process.env.BACKEND_URL}/api/auth/verify-email/${token}">Verify Email</a></p>
-     
-        <p><strong>Key Features and Benefits of Geisil:</strong></p>
-        <ul>
-          <li>Job Search & Applications: Candidates can explore and apply to a wide range of job opportunities.</li>
-          <li>Profile Management: Build and update a complete professional profile including education, skills, and work experience.</li>
-          <li>Job Posting: Employers and institutes can post jobs and connect with qualified candidates.</li>
-          <li>Candidate Verification: Companies and institutes can verify candidates who list them in their employment or education history.</li>
-          <li>Seamless Communication: Easy interaction between candidates and employers for smoother recruitment.</li>
-          <li>Secure Platform: Data protection and privacy ensured for both candidates and employers.</li>
-        </ul>
-     
-        <p>
-          We are confident that E2 Score will significantly improve your recruitment and job search experience by making the process faster, easier, and more reliable for both candidates and employers.
-        </p>
-     
-        <p>
-          For any assistance with the platform, including login issues or technical support, please contact our support team at:
-        </p>
-        <ul>
-          <li><strong>Email:</strong> <a href="mailto:info@geisil.com">info@geisil.com</a></li>
-          <li><strong>Phone:</strong> 9831823898</li>
-        </ul>
-     
-        <p>Thank you for choosing <strong>Global Employability Information Services India Limited</strong>.</p>
-        <p>We look forward to supporting your Job Searching and Job Posting needs.</p>
-     
-        <br />
-        <p>Sincerely,<br />
-        The Admin Team<br />
-        <strong>Global Employability Information Services India Limited</strong></p>
- 
-         <div style="text-align: center; margin-top: 30px;">
-      <img src="https://res.cloudinary.com/da4unxero/image/upload/v1746776002/QuikChek%20images/ntvxq8yy2l9de25t1rmu.png%22 alt="Footer" style="width:97px; height: 116px;" />
-    </div>
-      `,
-    };
-
-    await transporter.sendMail(mailOptions);
-
-    // User Account Creation Ends here
 
     // -----------------------------
     // Final Response
