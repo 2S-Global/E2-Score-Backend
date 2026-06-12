d7507473bbbe0754ff233808f7a2e0ef1c012cf9
2S-Global <2sglobaltechnologies@gmail.com>
2026-06-12 13:36:19 +0530

    UPDATE

--- routes/institute/instituteCampusRoute.js
+++ routes/institute/instituteCampusRoute.js
@@ -1,5 +1,5 @@
 import express from "express";
-
+import multer from "multer";
 import {
   addCampus,
   getCampuses,
@@ -7,13 +7,15 @@ import {
   updateCampus,
   deleteCampus,
 } from "../../controllers/institute/instituteCampusControllers.js";
-
+import userAuth from "../../middleware/authMiddleware.js";
+import Institutemid from "../../middleware/InstituteMiddleware.js";
+const upload = multer({ storage: multer.memoryStorage() });
 const router = express.Router();
 
-router.post("/", addCampus);
-router.get("/", getCampuses);
-router.get("/:id", getCampusById);
-router.put("/:id", updateCampus);
-router.delete("/:id", deleteCampus);
+router.post("/", userAuth, Institutemid, upload.none(), addCampus);
+router.get("/", userAuth, Institutemid, upload.none(), getCampuses);
+router.get("/:id", userAuth, Institutemid, upload.none(), getCampusById);
+router.put("/:id", userAuth, Institutemid, upload.none(), updateCampus);
+router.delete("/:id", userAuth, Institutemid, upload.none(), deleteCampus);
 
 export default router;
