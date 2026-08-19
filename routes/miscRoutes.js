5a195fff40bed6703724f52c79651d6c835d1a53
Mohan Das <137489225+mohandasss@users.noreply.github.com>
2026-08-19 16:31:29 +0530

    Update miscRoutes.js

--- routes/miscRoutes.js
+++ routes/miscRoutes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
 import { isPanAdded } from "../controllers/candidate/isPanAddedController.js";
-
+import userAuth from "../middleware/authMiddleware.js";
 const miscRouter = Router()
 
 
-miscRouter.get("/is-pan-added", isPanAdded)
+miscRouter.get("/is-pan-added", userAuth, isPanAdded)
 
 
 export default miscRouter
